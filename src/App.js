558b674c8e4ea67894fa02283488901edddab895
abhiagarwal18 <itsagarwal18@gmail.com>
2019-07-20 14:34:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,71 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import brace from 'brace';
 import AceEditor from 'react-ace';
-import fire from './Fire.js';
+import fire from './Fire';
 
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
-export class App extends Component{
-  constructor(props){
+export class App extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      code:''
+    this.state = {
+      code: ''
     };
   }
-  componentDidMount(){
+
+  componentDidMount() {
     fire
       .database()
       .ref('/codeBucket')
-      .on('value',snap=>{
-        this.setState({code:snap.val().code})
+      .on('value', snap => {
+        this.setState({ code: snap.val().code });
       });
   }
-onChange= codeInEditor=>{
-  console.log(codeInEditor)
-  let code=codeInEditor;
-  fire
-    .database()
-    .ref('/codeBucket')
-    .set({
-      code
-    });
-};
 
-btncheck=e=>{
+  onChange = codeInEditor => {
+    console.log(codeInEditor);
+    let code = codeInEditor;
+    fire
+      .database()
+      .ref('/codeBucket')
+      .set({
+        code
+      });
+  };
 
+  btncheck = e => {
+    let d;
+    if (e === 'js') {
+      d = `this is ${e} code`;
+      this.setState({ code: d });
+    } else if (e === 'ja') {
+      d = `this is ${e} code`;
+      this.setState({ code: d });
+    } else if (e === 'py') {
+      d = `this is ${e} code`;
+      this.setState({ code: d });
+    }
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <button onClick={() => this.btncheck('js')}>javascript</button>
+        <button onClick={() => this.btncheck('ja')}>java</button>
+        <button onClick={() => this.btncheck('py')}>python</button>
+
+        <AceEditor
+          mode='javascript'
+          theme='github'
+          onChange={this.onChange}
+          name='UNIQUE_ID_OF_DIV'
+          editorProps={{ $blockScrolling: true }}
+          value={this.state.code}
+        />
+      </Fragment>
+    );
+  }
 }
 
-
-render(){
-  return(
-    <Fragment>
-      <button onClick={()=>this.btncheck('js.')}
-    </Fragment>
-  )
-}
-
-}
+export default App;
