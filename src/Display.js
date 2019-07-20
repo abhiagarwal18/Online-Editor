91baec4c120e54a7d6ae2ed2aa25c34e01181d0c
abhiagarwal18 <itsagarwal18@gmail.com>
2019-07-20 14:30:30 +0530

    Display changes

--- src/Display.js
+++ src/Display.js
@@ -1,2 +1,48 @@
-import React, {Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
+export class Display extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      any: []
+    };
+  }
+
+  componentWillMount() {
+    console.log('will dis mount');
+  }
+
+  componentDidMount() {
+    console.log('did dis mount');
+  }
+
+  componentDidUpdate(prevprop, prevstate) {
+    console.log(prevprop);
+    console.log(prevstate);
+    console.log(this.state);
+    console.log(this.props);
+  }
+
+  change = () => {
+    this.setState({ any: [344, 45, 34] });
+  };
+
+  change2 = () => {
+    this.setState({ any: [4545, 4545, 3454454] });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <button onClick={this.change} />
+        </div>
+        <div>
+          <button onClick={this.change2} />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Display;
